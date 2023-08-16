c9dbc43c974af694a4e7667d44e9aa346c6e1f58
Daniel Hidalgo <dm080039@gmail.com>
2023-08-16 08:15:11 -0600

    Clase Administracion Servicios

--- src/AdministracionServicios/Servicios/AdministraciondeServicios.java
+++ src/AdministracionServicios/Servicios/AdministraciondeServicios.java
@@ -1,13 +1,44 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package AdministracionServicios.Servicios;
 
-/**
- *
- * @author Daniel Hidalgo
- */
+import java.util.ArrayList;
+import java.util.List;
+
 public class AdministraciondeServicios {
-    
+    private List<Servicios> servicios; // Declaración de la lista de servicios
+
+    public AdministraciondeServicios() {
+        servicios = new ArrayList<>(); // Inicialización de la lista de servicios en el constructor
+    }
+
+    public void agregarServicio(int codigo, String nombre, String descripcion, double precio) {
+        Servicios nuevoServicio = new Servicios(codigo, nombre, descripcion, precio);
+        servicios.add(nuevoServicio);
+    }
+
+    public void actualizarServicio(int codigo, String nuevaDescripcion, double nuevoPrecio) {
+        for (Servicios servicio : servicios) {
+            if (servicio.getCodigo() == codigo) {
+                servicio.setDescripcion(nuevaDescripcion);
+                servicio.setPrecio(nuevoPrecio);
+                break;
+            }
+        }
+    }
+
+    public void eliminarServicio(int codigo) {
+        servicios.removeIf(servicio -> servicio.getCodigo() == codigo);
+    }
+
+    public Servicios buscarServicio(int codigo) {
+        for (Servicios servicio : servicios) {
+            if (servicio.getCodigo() == codigo) {
+                return servicio;
+            }
+        }
+        return null;
+    }
+
+    public List<Servicios> getServicios() {
+        return servicios;
+    }
 }
